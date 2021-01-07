554998cd8816245ea5ae5117aeae1827df7b308e
nes <nes@pogi>
2021-01-07 16:51:02 -0500

    Move package for day 2

--- src/advent_of_code/day2/PasswordPolicyChecker.java
+++ src/advent_of_code/day2/PasswordPolicyChecker.java
@@ -1,4 +1,4 @@
-package advent_of_code;
+package advent_of_code.day2;
 
 import java.io.IOException;
 import java.nio.file.Files;
--- src/advent_of_code/day2/PasswordPolicyCheckerTest.java
+++ src/advent_of_code/day2/PasswordPolicyCheckerTest.java
@@ -1,4 +1,4 @@
-package advent_of_code;
+package advent_of_code.day2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
