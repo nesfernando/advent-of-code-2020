ce50eb65ec7dd1d4a50fc7ba50a7a5f4b8f24f1e
nes <nes@pogi>
2021-01-07 23:50:25 -0500

    Solution for day 3, part 1

---
+++ src/adventofcode/day3/TobogganMap.java
@@ -0,0 +1,31 @@
+package adventofcode.day3;
+
+import java.util.List;
+
+public class TobogganMap {
+
+  private List<TobogganMapRow> rows;
+  private int currentRow = 0;
+
+  public TobogganMap(List<TobogganMapRow> rows) {
+    this.rows = rows;
+  }
+
+  public void moveDown() {
+    int rowPosition = this.rows.get(this.currentRow).getPosition();
+    this.currentRow++;
+    this.rows.get(this.currentRow).setPosition(rowPosition);
+  }
+
+  public void moveRight(int positions) {
+    this.rows.get(this.currentRow).moveRight(positions);
+  }
+
+  public boolean isOpen() {
+    return this.rows.get(this.currentRow).isOpen();
+  }
+
+  public boolean isTree() {
+    return this.rows.get(this.currentRow).isTree();
+  }
+}
---
+++ src/adventofcode/day3/TobogganTrajectory.java
@@ -0,0 +1,39 @@
+package adventofcode.day3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TobogganTrajectory {
+  public static void main(String[] args) throws IOException {
+    List<String> lines = Files.readAllLines(Paths.get(args[0]));
+
+    var map = createMap(lines);
+
+    int numTrees = 0;
+
+    for (int i = 0; i < lines.size() - 1; i++) {
+      map.moveRight(3);
+      map.moveDown();
+
+      if (map.isTree()) {
+        numTrees++;
+      }
+    }
+
+    System.out.println("Number of trees encountered: " + numTrees);
+  }
+
+  private static TobogganMap createMap(List<String> lines) {
+    var rows = new ArrayList<TobogganMapRow>();
+
+    for (String s : lines) {
+      rows.add(new TobogganMapRow(s));
+    }
+
+    return new TobogganMap(rows);
+  }
+
+}
