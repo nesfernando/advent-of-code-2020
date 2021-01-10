b6be5bd5da99268b859a15ae5acdd9e80e12b2ad
nes <nes@pogi>
2021-01-10 18:17:53 -0500

    Generalize binary search for so we can use it for column finding.
    s/RowFinder/SeatFinder so we can combine column finding with it.

--- src/adventofcode/day5/SeatFinder.java
+++ src/adventofcode/day5/SeatFinder.java
@@ -1,19 +1,22 @@
 package adventofcode.day5;
 
-public class RowFinder {
+public class SeatFinder {
 
   private static int MAX_ROWS = 128;
 
   public static int findRowNumber(String string) {
+    return binaryFinder(string.replace('F', 'd').replace('B', 'u'), MAX_ROWS);
+  }
+
+  private static int binaryFinder(String string, int max) {
     int min = 0;
-    int max = MAX_ROWS;
 
     for (char direction : string.toCharArray()) {
       int offset = (int) Math.ceil((max - min) / 2.0);
-      if (direction == 'F') {
+      if (direction == 'd') {
         max -= offset;
       }
-      else if (direction == 'B') {
+      else if (direction == 'u') {
         min += offset;
       }
     }
--- src/adventofcode/day5/SeatFinderTest.java
+++ src/adventofcode/day5/SeatFinderTest.java
@@ -4,25 +4,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class RowFinderTest {
+class SeatFinderTest {
 
   @Test
   void findRow44() {
-    int rowNumber = RowFinder.findRowNumber("FBFBBFF");
+    int rowNumber = SeatFinder.findRowNumber("FBFBBFF");
 
     assertEquals(44, rowNumber);
   }
 
   @Test
   void findRow70() {
-    int rowNumber = RowFinder.findRowNumber("BFFFBBF");
+    int rowNumber = SeatFinder.findRowNumber("BFFFBBF");
 
     assertEquals(70, rowNumber);
   }
 
   @Test
   void findRow102() {
-    int rowNumber = RowFinder.findRowNumber("BBFFBB");
+    int rowNumber = SeatFinder.findRowNumber("BBFFBB");
 
     assertEquals(102, rowNumber);
   }
