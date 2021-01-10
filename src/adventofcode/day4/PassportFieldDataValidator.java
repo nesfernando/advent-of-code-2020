7a90147c68fb198bbc88ccd89f958dfde152f290
nes <nes@pogi>
2021-01-10 12:47:30 -0500

    Add validation for issue and expiration year fields.
    Add factory for valid passport data as test assembly utility.

--- src/adventofcode/day4/PassportFieldDataValidator.java
+++ src/adventofcode/day4/PassportFieldDataValidator.java
@@ -6,23 +6,29 @@ import java.util.function.Predicate;
 public class PassportFieldDataValidator {
 
   public static boolean validate(Map<String, String> passportData) {
-    return validate(passportData, "byr", PassportFieldDataValidator::isBirthYearValid);
+    return validateField(passportData, "byr", PassportFieldDataValidator::isBirthYearValid)
+        && validateField(passportData, "iyr", PassportFieldDataValidator::isIssueYearValid)
+        && validateField(passportData, "eyr", PassportFieldDataValidator::isExpirationYearValid);
   }
 
-  private static boolean validate(Map<String, String> passportData, String key, Predicate<String> predicate) {
-
+  private static boolean validateField(Map<String, String> passportData, String key, Predicate<String> predicate) {
     String value = passportData.get(key);
 
-    if (value == null)
-      return false;
-
-    return predicate.test(value);
+    return value != null && predicate.test(value);
   }
 
   private static boolean isBirthYearValid(String value) {
-    // specific to birth year
     int birthYear = Integer.parseInt(value);
     return 1920 <= birthYear && birthYear <= 2002;
   }
 
+  private static boolean isIssueYearValid(String value) {
+    int issueYear = Integer.parseInt(value);
+    return 2010 <= issueYear && issueYear <= 2020;
+  }
+
+  private static boolean isExpirationYearValid(String value) {
+    int expirationYear = Integer.parseInt(value);
+    return 2020 <= expirationYear && expirationYear <= 2030;
+  }
 }
--- src/adventofcode/day4/PassportFieldDataValidatorTest.java
+++ src/adventofcode/day4/PassportFieldDataValidatorTest.java
@@ -2,21 +2,71 @@ package adventofcode.day4;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 class PassportFieldDataValidatorTest {
 
-  @Test
-  void allFieldsValid() {
-//    var map = Map.ofEntries(
-//        Map.entry("pid", "087499704"), Map.entry("hgt", "74in"), Map.entry("ecl", "grn"), Map.entry("iyr", "2012"),
-//        Map.entry("eyr", "2030"), Map.entry("byr", "1980"), Map.entry("hcl", "#623a2f"));
-
-    var map = Map.ofEntries(Map.entry("byr", "1980"));
-
-    assertTrue(PassportFieldDataValidator.validate(map));
+  private static Map<String, String> getValidPassportData() {
+    return new HashMap<>(Map.ofEntries(
+        Map.entry("pid", "087499704"), Map.entry("hgt", "74in"), Map.entry("ecl", "grn"), Map.entry("iyr", "2012"),
+        Map.entry("eyr", "2030"), Map.entry("byr", "1980"), Map.entry("hcl", "#623a2f")));
   }
 
+  @Test
+  void allFieldsValid() {
+    var passportData = getValidPassportData();
+
+    assertTrue(PassportFieldDataValidator.validate(passportData));
+  }
+
+  @Test
+  void birthYearBefore1920Invalid() {
+    var passportData = getValidPassportData();
+    passportData.put("byr", "1919");
+
+    assertFalse(PassportFieldDataValidator.validate(passportData));
+  }
+
+  @Test
+  void birthYearAfter2002Invalid() {
+    var passportData = getValidPassportData();
+    passportData.put("byr", "2003");
+
+    assertFalse(PassportFieldDataValidator.validate(passportData));
+  }
+
+  @Test
+  void issueYearBefore2010Invalid() {
+    var passportData = getValidPassportData();
+    passportData.put("iyr", "2009");
+
+    assertFalse(PassportFieldDataValidator.validate(passportData));
+  }
+
+  @Test
+  void issueYearAfter2020Invalid() {
+    var passportData = getValidPassportData();
+    passportData.put("iyr", "2021");
+
+    assertFalse(PassportFieldDataValidator.validate(passportData));
+  }
+
+  @Test
+  void expirationYearBefore2020Invalid() {
+    var passportData = getValidPassportData();
+    passportData.put("eyr", "2019");
+
+    assertFalse(PassportFieldDataValidator.validate(passportData));
+  }
+
+  @Test
+  void expirationYearAfter2030Invalid() {
+    var passportData = getValidPassportData();
+    passportData.put("eyr", "2031");
+
+    assertFalse(PassportFieldDataValidator.validate(passportData));
+  }
 }
